913ed5c9e9472c64f900ac7dc946d772d82bccc5
osha7 <ogroetz@gmail.com>
2022-05-15 17:39:25 +0000

    tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
     @users = matching_users.order(:created_at)
-
-    render({ :template => "users_templates/all_users.html.erb"})
+    render({ :template => "user_templates/all_users.html.erb"})
   end
   
   def show
@@ -25,12 +24,12 @@ class UsersController < ApplicationController
   
   def update
     user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
-    the_user = matching_users.at(0)
-    
-    the_user.username = params.fetch("query_username")
+    matching_user = User.where({ :id => user_id })
+    the_user = matching_user.at(0)
+    new_username = params.fetch("query_username")
+    the_user.username = new_username
     the_user.save
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 
 end
